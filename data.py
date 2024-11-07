df479635fb9f0e0986cf7dfbb21733bf019f6ce6
LeoButsanets <99677173+LeoButsanets@users.noreply.github.com>
2024-11-07 09:31:26 +0100

    add data transform for the resnet18

--- data.py
+++ data.py
@@ -10,3 +10,10 @@ data_transforms = transforms.Compose(
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
+data_transforms_resnet18 = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
