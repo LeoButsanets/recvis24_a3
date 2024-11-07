683401741538cbd629ce28707a8c5ba6405c3a74
LeoButsanets <99677173+LeoButsanets@users.noreply.github.com>
2024-11-07 09:27:21 +0100

    Add resnet18 model

--- model_factory.py
+++ model_factory.py
@@ -1,8 +1,11 @@
 """Python file to instantite the model and the transform that goes with it."""
 
-from data import data_transforms
+from data import data_transforms, data_transforms_resnet18
 from model import Net
+import torchvision.models as models
+import torch.nn as nn
 
+nclasses = 500
 
 class ModelFactory:
     def __init__(self, model_name: str):
@@ -13,12 +16,20 @@ class ModelFactory:
     def init_model(self):
         if self.model_name == "basic_cnn":
             return Net()
+        
+        if self.model_name == "resnet18":
+            model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+            num_ftrs = model.fc.in_features
+            model.fc = nn.Linear(num_ftrs, nclasses)
+            return model
         else:
             raise NotImplementedError("Model not implemented")
 
     def init_transform(self):
         if self.model_name == "basic_cnn":
             return data_transforms
+        if self.model_name == "resnet18":
+            return data_transforms_resnet18
         else:
             raise NotImplementedError("Transform not implemented")
 
